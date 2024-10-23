008b02472f50247b4514f87c08dfe206c26fa97c
zuwhi <zuwhyyy@gmail.com>
2024-10-23 14:47:51 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+
 Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
 Route::middleware('auth')->group(function () {
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     });
-
-
-
 
 require __DIR__.'/auth.php';

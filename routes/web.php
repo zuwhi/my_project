4ffd46c045f70e05df384165229a1313332e921f
zuwhi <zuwhyyy@gmail.com>
2024-10-23 11:39:11 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,6 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-use App\Http\Controllers\ProfileController;
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-});
 
 
 require __DIR__.'/auth.php';
